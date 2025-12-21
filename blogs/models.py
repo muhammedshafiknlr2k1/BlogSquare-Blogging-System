b9898d60b60a1538d733b202b2d5da9c8b705f4a
SHAFI S <muhammedshafiknlr65@gmail.com>
2025-12-21 14:48:54 +0530

    category model created and registered in admin panel

---
+++ blogs/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    category_name = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
