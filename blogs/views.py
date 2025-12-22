fe8d8bedbb728eae53cca7f0e8466e9a10cea55f
SHAFI S <muhammedshafiknlr65@gmail.com>
2025-12-22 20:42:59 +0530

    404 custom error page created

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render, get_object_or_404
+from .models import Blogs, Category
+
 
 # Create your views here.
+
+def posts_by_category(request, category_id):
+    posts = Blogs.objects.filter(status='Published', category=category_id)
+    category = get_object_or_404(Category, pk=category_id)
+    context = {
+        'posts' : posts,
+        'category' : category
+    }
+    return render(request, 'posts_by_category.html', context)   
