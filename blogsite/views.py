ace28ba208ee7d13e30516581ad88514f942ba7f
SHAFI S <muhammedshafiknlr65@gmail.com>
2025-12-24 14:59:30 +0530

    make login required for accessing dashboards

--- blogsite/views.py
+++ blogsite/views.py
@@ -49,7 +49,7 @@ def login(request):
 
             if user is not None:
                 auth.login(request, user)
-            return redirect('home')
+            return redirect('dashboard')
 
     form = AuthenticationForm()
     context = {
--- dashboards/views.py
+++ dashboards/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from blogs.models import Category, Blogs
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required(login_url='login')
 def dashboard(request):
-    return render(request, 'dashboard/dashboard.html')
+    category_count = Category.objects.all().count()
+    blogs_count = Blogs.objects.all().count()
+    context = {
+        'category_count' : category_count,
+        'blogs_count' : blogs_count
+    }
+    return render(request, 'dashboard/dashboard.html', context)
